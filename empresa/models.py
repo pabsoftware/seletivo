f5e8655334fdc4aeaabbb4c5cb8f16b14e17ae9a
paulopab <pabsoftware@outlook.com>
2022-11-08 19:47:40 -0300

    Criado a model tecnologias em empresa

--- empresa/models.py
+++ empresa/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.tecnologia
+
 class Empresa(models.Model):
     choice_nicho_mercado = (
         ('M', 'Marketing'),
         ('N', 'Nutrição'),
         ('D', 'Design'),
     )
+    logo = models.ImageField(upload_to="logo_empresa")
     nome = models.CharField(max_length=40)
     email = models.CharField(max_length=40)
     cidade = models.CharField(max_length=40)
     endereco = models.CharField(max_length=40)
     caracteristicas_empresa = models.TextField()
     nicho_mercado= models.CharField(max_length=3, choices=choice_nicho_mercado)
+
+    def __str__(self):
+        return self.nome
