e261fecba6c2fad26b58297ad3b54f199bf7f594
paulopab <pabsoftware@outlook.com>
2022-11-08 19:40:53 -0300

    add atributo nicho de mercado no model empresa

--- empresa/models.py
+++ empresa/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Empresa(models.Model):
+    choice_nicho_mercado = (
+        ('M', 'Marketing'),
+        ('N', 'Nutrição'),
+        ('D', 'Design'),
+    )
     nome = models.CharField(max_length=40)
     email = models.CharField(max_length=40)
     cidade = models.CharField(max_length=40)
     endereco = models.CharField(max_length=40)
     caracteristicas_empresa = models.TextField()
+    nicho_mercado= models.CharField(max_length=3, choices=choice_nicho_mercado)
