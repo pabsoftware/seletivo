b3fc83ffe86f4919fba3a56f027fea0d5167eb63
paulopab <pabsoftware@outlook.com>
2022-11-11 14:56:43 -0300

    criado metodo nova vaga

--- seletivo/urls.py
+++ seletivo/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('empresa.urls')),
+    path('vagas/', include('vagas.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vagas/views.py
+++ vagas/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from empresa.models import Vagas
 
-# Create your views here.
+def nova_vaga(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        email = request.POST.get('email')
+        tecnologias_domina = request.POST.get('tecnologias_domina')
+        tecnologias_nao_domina = request.POST.get('tecnologias_nao_domina')
+        experiencia = request.POST.get('experiencia')
+        data_final = request.POST.get('data_final')
+        empresa = request.POST.get('empresa')
+        status = request.POST.get('status')
+        vaga = Vagas(
+                    titulo=titulo,
+                    email=email,
+                    nivel_experiencia=experiencia,
+                    data_final=data_final,
+                    empresa_id=empresa,
+                    status=status,
+        )
+        vaga.save()
+        
+    elif request.method == 'GET':
+        raise Http404()
+    return HttpResponse('Nova vaga')
