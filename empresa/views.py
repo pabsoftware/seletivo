1d5457ca3fd4d44277895728f9f80a4719b1fcce
pabsoftware <yopabsoftware@outlook.com>
2022-12-17 21:59:40 -0300

    formatacao codigo

--- empresa/views.py
+++ empresa/views.py
@@ -17,14 +17,14 @@ def nova_empresa(request):
 
     elif request.method =='POST':
       
-        nome = request.POST.get('nome')
-        email = request.POST.get('email')
-        cidade = request.POST.get('cidade')
-        endereco = request.POST.get('endereco')
-        nicho = request.POST.get('nicho')
+        nome            = request.POST.get('nome')
+        email           = request.POST.get('email')
+        cidade          = request.POST.get('cidade')
+        endereco        = request.POST.get('endereco')
+        nicho           = request.POST.get('nicho')
         caracteristicas = request.POST.get('caracteristicas')
-        tecnologias = request.POST.getlist('tecnologias') #getlist para pegar masi de um valor na lista
-        logo = request.FILES.get('logo')
+        tecnologias     = request.POST.getlist('tecnologias') #getlist para pegar masi de um valor na lista
+        logo            = request.FILES.get('logo')
 
         if (len(nome.strip()) == 0) or (len(email.strip()) == 0) or(len(cidade.strip()) == 0) or(len(endereco.strip()) == 0) or (len(nicho.strip()) == 0) or (len(caracteristicas.strip()) == 0):
             messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
